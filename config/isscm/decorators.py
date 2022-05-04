0745ec1fea065a67e0f092807948bb1a9c734557
chosss4@naver.com <happypp1@3>
2022-05-04 12:07:07 +0900

    발주 관련 기능 추가

--- config/isscm/decorators.py
+++ config/isscm/decorators.py
@@ -1,4 +1,6 @@
 from django.shortcuts import redirect
+from django.contrib import messages
+
 
 
 # 로그인 여부 데코레이터
@@ -9,5 +11,20 @@ def login_required(func):
         if login_session == '':
             return redirect('/accounts/login/')
 
+        return func(request, *args, **kwargs)
+    return wrapper
+
+# 사내 로그인 여부
+def login_ok(func):
+    def wrapper(request, *args, **kwargs):
+        login_session = request.session.get('login_session', '')
+
+        if login_session == '':
+            return redirect('/accounts/login/')
+        elif login_session == 'insung':
+            return func(request, *args, **kwargs)
+        else:
+            messages.info(request, "접근 권한이 없습니다.")
+            return redirect('/isscm/')
         return func(request, *args, **kwargs)
     return wrapper
--- config/isscm/urls.py
+++ config/isscm/urls.py
@@ -6,12 +6,18 @@ app_name = 'isscm'
 urlpatterns = [
     path('', views.index, name='index'),
     path('sheet_insert', views.sheet_insert, name='sheet_insert'),
+    path('order_insert', views.order_insert, name='order_insert'),
     path('sheet_list', views.sheet_list, name='sheet_list'),
+    path('order_list', views.order_list, name='order_list'),
     path('uploadFile/<int:pk>/', views.uploadFile, name='uploadFile'),
+    path('order_uploadFile/<int:pk>/', views.order_uploadFile, name='order_uploadFile'),
     path('sheet_detail/<int:pk>/', views.sheet_detail, name='sheet_detail'),
+    path('order_delete/<int:pk>/', views.order_delete, name='order_delete'),
+    path('order_modify/<int:pk>/', views.order_modify, name='order_modify'),
     path('sheet_modify/<int:pk>/', views.sheet_modify, name='sheet_modify'),
     path('sheet_delete/<int:pk>/', views.sheet_delete, name='sheet_delete'),
     path('searchResult/', views.searchResult, name='searchResult'),
+    path('ordersearchResult/', views.ordersearchResult, name='ordersearchResult'),
 
 ]
 

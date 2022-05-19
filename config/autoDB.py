703a8309bdc68154a1361aa19ee7a040cbacd5be
chosss4@naver.com <happypp1@3>
2022-05-19 16:08:47 +0900

    자동업데이트 재업로드

--- config/autoDB.py
+++ config/autoDB.py
@@ -1,10 +1,13 @@
 import os
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 import django
+
 django.setup()
 
 import sys
+
 sys.path.append('../..')
 from isscm.models import Ordersheet, EstimateSheet
 from datetime import date, timedelta
@@ -12,21 +15,11 @@ from dateutil.relativedelta import relativedelta
 from django.shortcuts import get_object_or_404
 
 
-
-
-
-
-
 def auto_db_update():
+    or_month = Ordersheet.objects.filter(rp_date__lte=date.today() - relativedelta(days=3))
+    or_month.update(new_old='기존')
 
-    or_month = get_object_or_404(Ordersheet, rp_date__lte=date.today() - relativedelta(day=3))
-    es_month = get_object_or_404(EstimateSheet, no=or_month.essheet_pk)
-
-    es_month.new_old = '기존'
-    es_month.save()
-
-    or_month.new_old = '기존'
-    or_month.save()
+    es_month = EstimateSheet.objects.filter(rp_date__lte=date.today() - relativedelta(days=3), finish="종료")
+    es_month.update(new_old='기존')
 
 auto_db_update()
-
